cf174b4b84ef954ec4dda5416241c5e8cb7c9293
unai <unaicc2003@gmail.com>
2024-09-17 16:59:27 +0200

    compose

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM node
-RUN mkdir -p /usr/src/app
-COPY . /usr/src/app
+RUN mkdir -p /home/app
+WORKDIR /home/app
 EXPOSE 3000
 CMD ["node", "/usr/src/app/index.js"]
--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const app = express();
 mongoose.connect('mongodb://unai:011110@mongo:27017/miapp?authSource=admin', );
 
 app.get('/', async (req, res) => {
-    console.log('listando');
+    console.log('listando animales');
     const animales = await Animal.find();
     res.send(animales);
 });
@@ -23,7 +23,7 @@ app.get('/crear', async (req, res) => {
         type: 'felino'
     });
     await animal.save();
-    res.send('animal creado');
+    res.send('animal creado con exito');
 });
 
 app.listen(3000, () => console.log('server running on port 3000'));
